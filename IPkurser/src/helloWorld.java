5bc9e45b1f1ee04f1b7007a31f999440aa0979ea
JohnnyStuttgart <jonny.nylund@gmail.com>
2011-11-15 17:07:58 +0100

    hello again

---
+++ IPkurser/src/helloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonnynylund
+ */
+public class helloWorld {  
+        public static void main(String args[])
+        {
+           System.out.println("Hello World!");
+        }
+}
+
