7b0272851a6e5da122e033462ca8e24efb8fa214
Mattias Hemmingsson <mattias@fo-tp-matte.(none)>
2011-11-15 17:29:13 +0100

    nu mother fucker

---
+++ ipkurs1/src/Ettan/nura.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ettan;
+
+/**
+ *
+ * @author mattias
+ */
+public class nura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hej");
+    }
+}
